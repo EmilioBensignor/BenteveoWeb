58e5360d829dc626824fb37def5ff23fce516f4a
Emilio Bensignor <liobensignor@gmail.com>
2024-12-16 13:14:45 -0300

    Projects Maquetado en Mobile

--- shared/projects.js
+++ shared/projects.js
@@ -2,81 +2,81 @@ export const projects = [
   {
     title: "Corro con Jonás",
     text: "Una carrera, un corredor y un mensaje oculto.",
-    brand: "/content/img/logo-brand.png",
+    brand: "logo-brand",
     altBrand: "Image",
-    img: "/content/img/home/proyectos/corro-con-jonas.webp",
+    img: "corro-con-jonas",
     altImg: "Campaña Corro con Jonas",
     link: "case-details.html"
   },
   {
     title: "Showroom Virtual",
     text: "Experiencia inmersiva 4D.",
-    brand: "/content/img/logo-brand2.png",
+    brand: "logo-brand2",
     altBrand: "Image",
-    img: "/content/img/home/proyectos/revear-showroom-virtual.webp",
+    img: "revear-showroom-virtual",
     altImg: "Revear Showroom Virtual",
     link: "case-details.html"
   },
   {
     title: "Sabiduría al Plato",
     text: "Campaña 360° con Rubén 'el negro' Rada.",
-    brand: "/content/img/logo-brand3.png",
+    brand: "logo-brand3",
     altBrand: "Image",
-    img: "/content/img/home/proyectos/paty-sabiduria-al-plato.webp",
+    img: "paty-sabiduria-al-plato",
     altImg: "Paty Sabiduría al Plato",
     link: "case-details.html"
   },
   {
     title: "Descubre una Vida",
     text: "Flashmob inspirado en el equilibrio, la liviandad y la vitalidad de Qualy.",
-    brand: "/content/img/logo-brand2.png",
+    brand: "logo-brand2",
     altBrand: "Image",
-    img: "/content/img/home/proyectos/descubre-una-vida-qualy.webp",
+    img: "descubre-una-vida-qualy",
     altImg: "Descubre una Vida Qualy",
     link: "case-details.html"
   },
   {
     title: "Sadía presenta 'Panchisimo'",
     text: "Spot publicitario para nueva línea de salchichas estilo alemanas.",
-    brand: "/content/img/logo-brand3.png",
+    brand: "logo-brand3",
     altBrand: "Image",
-    img: "/content/img/home/proyectos/sadia-presenta-panchismo.webp",
+    img: "sadia-presenta-panchismo",
     altImg: "Sadía Presenta Panchisimo",
     link: "case-details.html"
   },
   {
     title: "Man in Black",
     text: "HTML, Development",
-    brand: "/content/img/logo-brand3.png",
+    brand: "logo-brand3",
     altBrand: "Image",
-    img: "/content/img/works06.jpg",
+    img: "works06",
     altImg: "Image",
     link: "case-details.html"
   },
   {
     title: "Old School",
     text: "App Design, Development",
-    brand: "/content/img/logo-brand.png",
+    brand: "logo-brand",
     altBrand: "Image",
-    img: "/content/img/works07.jpg",
+    img: "works07",
     altImg: "Image",
     link: "case-details.html"
   },
   {
     title: "Paper Girl",
     text: "UI-UX Design, HTML, Development",
-    brand: "/content/img/logo-brand2.png",
+    brand: "logo-brand2",
     altBrand: "Image",
-    img: "/content/img/works08.jpg",
+    img: "works08",
     altImg: "Image",
     link: "case-details.html"
   },
   {
     title: "Moto Wheel",
     text: "Branding, Print Design",
-    brand: "/content/img/logo-brand3.png",
+    brand: "logo-brand3",
     altBrand: "Image",
-    img: "/content/img/works09.jpg",
+    img: "works09",
     altImg: "Image",
     link: "case-details.html"
   }
